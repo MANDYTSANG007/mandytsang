5b0ecc4bbaea76efe74885d0eb791c78b0c270f8
Mandy Tsang <mandy.tsang007@gmail.com>
2023-02-11 15:55:31 -0800

    Added favicon.png and navbar styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
@@ -8,13 +9,15 @@ import Contact from './components/Contact/Contact';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <>
-            <nav>
-                <h1> Navbar</h1>
-            </nav>
-        </>
+        <nav className="navbar">
+            <ul className="directories">
+                <li><Link to="/"> Home </Link></li>
+                <li><Link to="/portfolio"> Portfolio </Link></li>
+                <li><Link to="/contact"> Contact </Link></li>
+            </ul>
+        </nav>
     )
 }
 export default Navbar;
