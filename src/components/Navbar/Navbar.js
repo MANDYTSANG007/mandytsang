a6d8e4a267259960e914cbad6ee1e2947b17cdbf
Mandy Tsang <mandy.tsang007@gmail.com>
2023-02-11 17:36:47 -0800

    Added media queries to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
     return (
         <nav className="navbar">
+            <img
+                src={require(`../../assets/${navbar ? "x" : "menu"}.png`)}
+                alt=""
+                className="menu-icon"
+                onClick={() => {
+                    setNavbar(!navbar);
+                }}
+            />
             <ul className="directories">
                 <li><Link to="/"> Home </Link></li>
                 <li><Link to="/portfolio"> Portfolio </Link></li>
                 <li><Link to="/contact"> Contact </Link></li>
             </ul>
+            <div className="mobile-navbar" style={{ left: navbar ? "0%" : "-70% " }}>
+                <p style={{ padding: "0vh 3vh"}}>Menu</p>
+                <ul className="mobile-directories">
+                    <li><Link to="/"> Home </Link></li>
+                    <li><Link to="/portfolio"> Portfolio </Link></li>
+                    <li><Link to="/contact"> Contact </Link></li>
+                </ul>
+            </div>
         </nav>
     )
 }
