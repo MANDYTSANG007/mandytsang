a11365326f2e8cf02dfa2dd6a2f7fe4b19a100f7
Mandy Tsang <mandy.tsang007@gmail.com>
2023-02-12 20:53:16 -0800

    Initial setup for the about section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,12 +6,20 @@ const Home = () => {
         <main className="home-main">
             <div className="home-background">
                 <img src={require("../../assets/curiosity.jpeg")} alt="" className="background-img" />
-                <p className="home-heading1"> Mandy Tsang </p>
-                <p className="home-heading2">
+                <h1 className="home-heading1"> Mandy Tsang </h1>
+                <h3 className="home-heading2">
                     <span> Full Stack Developer </span>
                     <span>  |  </span>
                     <span> Data Analyst </span>
-                </p>
+                </h3>
+                <section className="about">
+                    <article className="about-container">
+                        <h1> About </h1>
+                        <div className="intro-info">
+                            <p>I am a full stack developer who loves to build apps.</p>
+                        </div>
+                    </article>
+                </section>
             </div>
         </main>
     )
