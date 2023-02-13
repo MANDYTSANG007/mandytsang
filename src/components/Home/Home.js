5b1de37221e3d9403a368e209a02ea0f55520733
Mandy Tsang <mandy.tsang007@gmail.com>
2023-02-12 20:28:44 -0800

    Added media query for iPad.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,14 +6,12 @@ const Home = () => {
         <main className="home-main">
             <div className="home-background">
                 <img src={require("../../assets/curiosity.jpeg")} alt="" className="background-img" />
-                <div className="home-text">
-                    <h1 className="home-heading" style={{ paddingBottom: "10px"}}>Mandy Tsang</h1>
-                    <h3>
-                        <span>Full Stack Developer</span>
-                        <span>  |  </span>
-                        <span>Data Analyst</span>
-                    </h3>
-                </div>
+                <p className="home-heading1"> Mandy Tsang </p>
+                <p className="home-heading2">
+                    <span> Full Stack Developer </span>
+                    <span>  |  </span>
+                    <span> Data Analyst </span>
+                </p>
             </div>
         </main>
     )
