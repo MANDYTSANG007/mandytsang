e292d6754f94aca199ff4c8f227acfd11d439fcd
Mandy Tsang <mandy.tsang007@gmail.com>
2023-02-14 17:19:19 -0800

    Added overlay feature when image is hovered.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,6 +5,11 @@ const Card = (props) => {
     return (
         <div className="card">
             <img className="card-img" alt={props.title} src={props.image} />
+            <div className="overlay">
+                <div className="overlay-content">
+                    {props.title}
+                </div>
+            </div>
         </div>
     )
 }
